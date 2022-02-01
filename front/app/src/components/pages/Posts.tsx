5456935a045486c1f18110c5f518b8dbaa72bb9b
qunitaros <kunitaros8585@icloud.com>
2022-02-01 18:05:21 +0900

    create post api

--- front/app/src/components/pages/Posts.tsx
+++ front/app/src/components/pages/Posts.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-import { makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
@@ -12,11 +12,20 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import DialogActions from "@material-ui/core/DialogActions";
 
-import { Post, User } from "../../interfaces";
-import { getPosts } from "../../lib/api/post";
+import { CreatePostFormData, Post } from "../../interfaces";
+import { createPost, getPosts } from "../../lib/api/post";
 import { fields } from "../../data/fields";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import AlertMessage from "../utils/AlertMessage";
+import { AuthContext } from "../../App";
 
 const useStyles = makeStyles({
   card: {
@@ -49,10 +58,29 @@ const inisialState: Post = {
 const Posts: React.FC = () => {
   const classes = useStyles();
 
+  const { currentUser } = useContext(AuthContext);
+  const history = useHistory();
+
   const [post, setPost] = useState<Post>(inisialState);
   const [posts, setPosts] = useState<Post[]>([]);
+
   const [loading, setLoading] = useState<boolean>(true);
   const [postDetailOpen, setPostDetailOpen] = useState<boolean>(false);
+  const [postField, setPostField] = useState<number>();
+  const [content, setContent] = useState<string>("");
+  const [alertMessageOpen, setAlertMessageOpen] = useState(false);
+  const [createPostFormOpen, setCreatePostFormOpen] = useState(false);
+  const [update, setUpdata] = useState<boolean>(false);
+
+  const createFormData = (): CreatePostFormData => {
+    const formData = new FormData();
+
+    formData.append("userId", String(currentUser.id));
+    formData.append("content", content);
+    formData.append("postField", String(postField));
+
+    return formData;
+  };
 
   // Post一覧を取得する
   const handleGetPosts = async () => {
@@ -71,6 +99,31 @@ const Posts: React.FC = () => {
     setLoading(false);
   };
 
+  //Post投稿機能
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const data = createFormData();
+
+    try {
+      const res = await createPost(data);
+
+      if (res?.status === 200) {
+        setCreatePostFormOpen(false);
+        history.push("/posts");
+
+        setContent("");
+        setPostField(0);
+
+        console.log("Create post successfully!");
+      } else {
+        setAlertMessageOpen(true);
+      }
+    } catch (err) {
+      console.log(err);
+      setAlertMessageOpen(true);
+    }
+  };
+
   useEffect(() => {
     handleGetPosts();
   }, []);
@@ -84,58 +137,69 @@ const Posts: React.FC = () => {
     <>
       {!loading ? (
         posts?.length > 0 ? (
-          <Grid container justifyContent="center" spacing={2}>
-            {posts?.map((post: Post, index: number) => {
-              return (
-                <Grid key={index} item xs={4}>
-                  <Card
-                    className={classes.card}
-                    variant="outlined"
-                    style={{
-                      backgroundColor: "#cebc86",
-                      color: "inhedit",
-                      opacity: "0.5",
-                    }}
-                  >
-                    <CardContent>
-                      <Link to={`user/${post.userId}`}>
-                        <CardHeader avatar={<Avatar src={post.image.url} />} />
-                      </Link>
-                      <CardMedia style={{ height: "100px" }} />
-                      <Typography
-                        className={classes.title}
-                        color="textSecondary"
-                        gutterBottom
-                      >
-                        {post.userName}
-                      </Typography>
-                      <Typography variant="h5" component="h2">
-                        {post.content}
-                      </Typography>
-                      <Typography
-                        className={classes.postContent}
-                        color="textSecondary"
-                      >
-                        {post.content}
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        ニャースニャース
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                  <Button
-                    size="small"
-                    onClick={() => {
-                      setPost(post);
-                      setPostDetailOpen(true);
-                    }}
-                  >
-                    詳しく見る
-                  </Button>
-                </Grid>
-              );
-            })}
-          </Grid>
+          <>
+            <Button
+              variant="contained"
+              onClick={() => setCreatePostFormOpen(true)}
+              style={{ marginBottom: 30, width: "30%" }}
+            >
+              新しく投稿する
+            </Button>
+            <Grid container justifyContent="center" spacing={2}>
+              {posts?.map((post: Post, index: number) => {
+                return (
+                  <Grid key={index} item xs={4}>
+                    <Card
+                      className={classes.card}
+                      variant="outlined"
+                      style={{
+                        backgroundColor: "#cebc86",
+                        color: "inhedit",
+                        opacity: "0.5",
+                      }}
+                    >
+                      <CardContent>
+                        <Link to={`user/${post.userId}`}>
+                          <CardHeader
+                            avatar={<Avatar src={post.image.url} />}
+                          />
+                        </Link>
+                        <CardMedia style={{ height: "100px" }} />
+                        <Typography
+                          className={classes.title}
+                          color="textSecondary"
+                          gutterBottom
+                        >
+                          {post.userName}
+                        </Typography>
+                        <Typography variant="h5" component="h2">
+                          {post.content}
+                        </Typography>
+                        <Typography
+                          className={classes.postContent}
+                          color="textSecondary"
+                        >
+                          {post.content}
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                          ニャースニャース
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                    <Button
+                      size="small"
+                      onClick={() => {
+                        setPost(post);
+                        setPostDetailOpen(true);
+                      }}
+                    >
+                      詳しく見る
+                    </Button>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </>
         ) : (
           <Typography component="p" variant="body2" color="textSecondary">
             投稿されていません。
@@ -165,6 +229,61 @@ const Posts: React.FC = () => {
           </Grid>
         </DialogContent>
       </Dialog>
+      <form noValidate autoComplete="off">
+        <Dialog
+          open={createPostFormOpen}
+          keepMounted
+          onClose={() => setCreatePostFormOpen(false)}
+        >
+          <DialogTitle style={{ textAlign: "center" }}>新しい投稿</DialogTitle>
+          <DialogContent>
+            <FormControl variant="outlined" margin="dense" fullWidth>
+              <InputLabel id="demo-simple-select-outlined-label">
+                分野
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={postField}
+                onChange={(e: React.ChangeEvent<{ value: unknown }>) =>
+                  setPostField(e.target.value as number)
+                }
+                label="分野"
+              >
+                {fields.map((field, index) => (
+                  <MenuItem key={index} value={index}>
+                    {field}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              placeholder="140文字以内で書いてください。"
+              variant="outlined"
+              multiline
+              fullWidth
+              label="勉強したことや教わりたいこと"
+              rows="6"
+              value={content}
+              margin="dense"
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setContent(e.target.value);
+              }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleSubmit} color="secondary">
+              送信
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </form>
+      <AlertMessage // エラーが発生した場合はアラートを表示
+        open={alertMessageOpen}
+        setOpen={setAlertMessageOpen}
+        severity="error"
+        message="投稿に失敗しました。"
+      />
     </>
   );
 };
