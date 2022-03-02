bd744de2e3f5f39d3d579d26055b97954745c947
qunitaros <kunitaros8585@icloud.com>
2022-03-02 12:33:49 +0900

    [add]development.rbにホスト情報の追加

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -66,4 +66,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #ホストの設定
+  config.hosts << "54.150.100.46"
 end
