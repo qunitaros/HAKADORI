2df839ee16de8bd790281aa08b177dff644e8428
qunitaros <kunitaros8585@icloud.com>
2022-03-06 13:43:25 +0900

    [fix]healthcheckの修正

--- api/app/controllers/api/v1/health_check_controller.rb
+++ api/app/controllers/api/v1/health_check_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::HealthCheckController < ApplicationController
   def index 
-    head 200
-    render :json { status: 200, message: "アクセス成功" }
+    render :json { status: 200, message: "アクセス成功", head 200 }
   end
 end
