3e92deed7cf0aecbcf824f552fca30b680aa52e3
qunitaros <kunitaros8585@icloud.com>
2022-03-08 10:31:23 +0900

    [add]seedにpostデータの投入

--- api/db/seeds/development/posts.rb
+++ api/db/seeds/development/posts.rb
@@ -1,75 +1,75 @@
 posts = [
   {
     content: "毎週休日はカフェで勉強するようにしています!いつも一人なので暇な方いたらぜひご一緒しましょう!",
-    image: ""
+    image: "public/post_images/post_english.jpg"
   },
   {
     content: "Vue.jsならある程度教えられるレベルなのでわからないことがある人はどんどん言ってきてください!",
-    image: ""
+    image: "public/post_images/post_programing.jpg"
   },
   {
     content: "デザインを勉強すればするほど、結局余白が一番大事なんだなぁ",
-    image: ""
+    image: "public/post_images/post_design.jpg"
   },
   {
     content: "始めました。よろしくお願いします。",
-    image: ""
+    image: nil
   },
   {
     content: "WordPress導入のためPHP勉強中です!同じような方はいいねください!",
-    image: ""
+    image: "public/post_images/post_blog.jpg"
   },
   {
     content: "アナリティクスとか数字関係難しいなあ、、",
-    image: ""
+    image: "public/post_images/post_sns.jpg"
   },
   {
     content: "動画編集者あるある「高単価案件取りたいのに、高単価で提案したことがない」",
-    image: ""
+    image: "public/post_images/post_movie.jpg"
   },
   {
     content: "書店で自分の持ってる資格の参考書エリアに行くと何か嬉しくなりますよね笑笑",
-    image: ""
+    image: "public/post_images/post_finance.jpg"
   },
   {
     content: "髪色派手な人って銀色のネイルしている人多いですよね。やっぱ髪色との兼ね合いも考えなきゃなのか。。。",
-    image: ""
+    image: "public/post_images/post_neil.jpg"
   },
   {
     content: "カウンセラーって、小さい頃からイイ子ちゃんしてきたでしょってクライアントに伝えてる人多いと思うけど、実はカウンセラーこそ、クライアントにいいカウンセラーだと思われたくて「仮面」付けてるんだよねぇ…
     ",
-    image: ""
+    image: nil
   },
   {
     content: "椎間板は運動が栄養ということを初めて知って驚愕（血液からの栄養は来ない）運動が必要なのはこれだったんですね。",
-    image: ""
+    image: "public/post_images/post_medicine.jpg"
   },
   {
     content: "「議事録」ってビジネススキル問われる仕事だよなぁ…と他人が書いた議事録を読んで染み染み感じます。
     長い。長すぎる。まとめろ。簡潔に。",
-    image: ""
+    image: "public/post_images/post_bussiness.jpg"
   },
   {
     content: "タスク管理の絶対的な掟として「タスク管理自体をタスクにしない」というものがあります。営業関係はかなり教えられると思うので、是非いいねください!!!!",
-    image: ""
+    image: "public/post_images/post_sales.jpg"
   },
   {
     content: "プレゼン資料で字に色をつけるな派と重要なところに色つけろ派のどっちのご機嫌とるかの二択、皆さんはどうしてますか？",
-    image: ""
+    image: nil
   },
   {
     content: "積み立てNisa、設定をミスって分配金を受取型にしてしまった、、、",
-    image: ""
+    image: nil
   },
   {
     content: "本で学んで、周りより分かった気でいる。けど実際商品売るとなったら何も出来ない。多分マーケティングチームの会議に入っても、情報頭に入れるだけで精一杯になります。",
-    image: ""
+    image: "public/post_images/post_market.jpg"
   },
   {
     content: "カフェ勉時、マーカーだらけの六法のページ開け、カッコつけちゃって、、、
     しまいには周りの目線気にして、あえて煮詰まった時の渋い顔する
     カッコつけてボールペン回しすぎて失敗し落下",
-    image: ""
+    image: "public/post_images/post_low.jpg"
   }
 ]
 
@@ -77,12 +77,20 @@ posts.length.times do |n|
   content = posts[n][:content]
   image = posts[n][:image]
 
-  Post.create!(
-    user_id: n + 1,
-    content: content,
-    post_field: n,
-    post_image:File.open(image)
-  )
+  if image == nil
+    Post.create!(
+      user_id: n + 1,
+      content: content,
+      post_field: n
+    )
+  else
+    Post.create!(
+      user_id: n + 1,
+      content: content,
+      post_field: n,
+      post_image:File.open(image)
+    )
+  end
 end
 
 puts "posts = #{Post.count}"
--- api/db/seeds/production/users.rb
+++ api/db/seeds/production/users.rb
@@ -1,88 +1,88 @@
 users = [
   { name: "yuji", 
-    prof:  "初めまして、yuujiです。将来海外で働いてみたいと思い、最近英語を勉強し始めました。是非とも一緒に勉強しましょう!", 
+    prof:  "初めまして、yujiです。将来海外で働いてみたいと思い、最近英語を勉強し始めました。是非とも一緒に勉強しましょう!", 
     gender: 0,
-    image: "../../public/icons/english_man.jpg"
+    image: "public/icons/english_man.jpg"
   },
   { name: "恵", 
     prof:  "エンジニアです。主にフロント部分を触ってます。" ,
     gender: 0,
-    image: "../../public/icons/programing_man.jpg"
+    image: "public/icons/programing_man.jpg"
   },
   { name: "ゆうた", 
     prof:  "デザインの勉強をしています。人見知りなのですが、よろしくお願いいたします。" ,
     gender: 0,
-    image: "../../public/icons/design_man.jpg"
+    image: "public/icons/design_man.jpg"
   },
   { name: "トゲ", 
     prof:  "ライター",
     gender: 0, 
-    image: "../../public/icons/writer_man.jpg"
+    image: "public/icons/writer_man.jpg"
   },
   { name: "悟", 
     prof:  "ブログ始めました!まだまだ初心者なので詳しい方と一緒に勉強できれば幸いです!" ,
     gender: 0,
-    image: "../../public/icons/blog_man.jpg"
+    image: "public/icons/blog_man.jpg"
   },
   { name: "panda", 
     prof:  "よろしくお願います!!!!!インフルエンサーになりたいです!!!!!" ,
     gender: 0,
-    image: "../../public/icons/panda_man.jpg"
+    image: "public/icons/panda_man.jpg"
   },
   { name: "葵", 
     prof:  "葵です。会社で動画を編集する仕事が回ってきたので急遽勉強しています。" ,
     gender: 0,
-    image: "../../public/icons/movie_man.jpg"
+    image: "public/icons/movie_man.jpg"
   },
   { name: "七海建人", 
     prof:  "ファイナンシャルプランナーになりたくてファイナンス周りを勉強しています。アウトプットを兼ねて誰かに教えてみたいです。" ,
     gender: 0,
-    image: "../../public/icons/finace_man.jpg"
+    image: "public/icons/finance_man.jpg"
   },
   { name: "のばら", 
     prof:  "今まで身だしなみに気をつかってこなかったのですが、最近ネイルやメイクの勉強をしています。皆さんがどのようにしているのか知りたいです!" ,
     gender: 1,
-    image: "../../public/icons/neil_woman.jpg"
+    image: "public/icons/neil_woman.jpg"
   },
   { name: "まき", 
     prof:  "カウンセラーを目指して日々勉強中です。SNSなどでアウトプットしているのですが、対面でもアウトプットしたいと考えています。" ,
     gender: 1,
-    image: "../../public/icons/counselor_woman.jpg"
+    image: "public/icons/counselor_woman.jpg"
   },
   { name: "祥子", 
     prof:  "初めまして!,健康やストレッチの勉強をしています!プロの療法士の方と喋ってみたいです!できればメンターを頼みたいです!" ,
     gender: 1,
-    image: "../../public/icons/medicine_woman.jpg"
+    image: "public/icons/medicine_woman.jpg"
   },
   { name: "アカリ", 
     prof:  "企画の仕事をしていて、休日は主にその勉強をしています。いつも一人で退屈一緒に勉強しませんか？" ,
     gender: 1,
-    image: "../../public/icons/plan_woman.jpg"
+    image: "public/icons/plan_woman.jpg"
   },
   { name: "桃", 
     prof:  "営業をしており、休日は営業日記などを眺めてアウトプットしています。" ,
     gender: 1,
-    image: "../../public/icons/salse_woman.jpg"
+    image: "public/icons/sales_woman.jpg"
   },
   { name: "まいちゃん", 
     prof:  "プレゼン資料が見にくい!!を上司に怒られてしまいました。上手に作れるようになりたい!" ,
     gender: 1,
-    image: "../../public/icons/presen_woman.jpg"
+    image: "public/icons/presen_woman.jpg"
   },
   { name: "歌姫", 
     prof:  "将来のことが不安で、積み立てなどの勉強を始めました。ゆくゆくは株式にも力を入れてみたいです!" ,
     gender: 1,
-    image: "../../public/icons/money_woman.jpg"
+    image: "public/icons/money_woman.jpg"
   },
   { name: "めいめい", 
     prof:  "広報部の仕事をしているのですが、毎日勉強しながらなんとか仕事について行っている状態です。教えてくれる人がいれば幸いです。" ,
     gender: 1,
-    image: "../../public/icons/market_woman.jpg"
+    image: "public/icons/market_woman.jpg"
   },
   { name: "yuki", 
     prof:  "司法試験の勉強で毎日部屋にこもって勉強しています。いつも一人で寂しい上に全然捗らないので始めました。SNSで毎日アウトプットしているのですが、対面でもアウトプットしていきたいです。" ,
     gender: 1,
-    image: "../../public/icons/low_woman.jpg"
+    image: "public/icons/low_woman.jpg"
   }
 ]
 
@@ -99,19 +99,19 @@ users.length.times do |n|
   profile = users[n][:prof]
   image = users[n][:image]
 
-  if user.new_record?
-    user.name = name
-    user.password = "password"
-    user.password_confirmation = "password"
-    user.prefecture = prefecture
-    user.gender = gender
-    user.field = n
-    user.day_off = day_off
-    user.birthday = date
-    user.profile = profile
-    user.image:File.open(image)
-    user.save!
-  end
+  User.create!(
+    name: name,
+    email: email,
+    password: "password",
+    password_confirmation: "password",
+    prefecture: prefecture,
+    gender: gender,
+    field: n,
+    day_off: day_off,
+    birthday: date,
+    profile: profile,
+    image:File.open(image)
+  )
 end
 
 
